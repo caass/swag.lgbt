411b8e62d88522fea4e7c0ab74dbb697c18a4400
Cass Fridkin <cass@cloudflare.com>
2022-04-15 16:24:40 -0500

    cleanup resize-image

--- functions/api/resize-image.ts
+++ functions/api/resize-image.ts
@@ -1,26 +1,13 @@
-function getImageParam<T extends keyof RequestInitCfPropertiesImage>(
-  url: URL,
-  property: T
-): RequestInitCfPropertiesImage[T] {
-  if (!url.searchParams.has(property)) {
-    return undefined;
-  }
+/**
+ * Necessary for Next.js to deploy -- a small api endpoint that does image
+ * resizing in workers
+ */
+export const onRequest: PagesFunction = async ({ request }) => {
+  const { url: urlstring, headers } = request;
 
-  const value = url.searchParams.get(property);
-  if (value === null) {
-    return undefined;
-  }
-
-  return value as RequestInitCfPropertiesImage[typeof property];
-}
-
-const resizeImage = async ({
-  url: urlstring,
-  headers,
-}: Request): Promise<Response> => {
   const url = new URL(urlstring);
   const imageURL = url.searchParams.get("image");
-  if (imageURL === null) {
+  if (!imageURL) {
     throw new Error(`Did not receive image URL in ${url}`);
   }
 
@@ -41,6 +28,21 @@ const resizeImage = async ({
   );
 };
 
-export const onRequest: PagesFunction = async ({ request }) => {
-  return resizeImage(request);
-};
+/**
+ * Small helper function to retrieve an `cf.image` property from a URL
+ */
+function getImageParam<T extends keyof RequestInitCfPropertiesImage>(
+  url: URL,
+  property: T
+): RequestInitCfPropertiesImage[T] {
+  if (!url.searchParams.has(property)) {
+    return undefined;
+  }
+
+  const value = url.searchParams.get(property);
+  if (value === null) {
+    return undefined;
+  }
+
+  return value as RequestInitCfPropertiesImage[typeof property];
+}
