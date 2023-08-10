11da9f298e5dfad1dd9507be92984ca702d6610e
Cass Fridkin <cass@swag.lgbt>
2023-08-10 13:00:17 -0400

    add some import stuff, stop before order tho

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,16 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:qwik/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript"
   ],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+    "import/extensions": [".ts", ".tsx"],
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
@@ -23,6 +32,9 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
+    "no-case-declarations": "off",
+    "no-console": "off",
+
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-empty-interface": "off",
@@ -31,9 +43,10 @@ module.exports = {
     "@typescript-eslint/no-this-alias": "off",
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    "no-case-declarations": "off",
-    "no-console": "off",
     "@typescript-eslint/consistent-type-imports": "warn",
     "@typescript-eslint/no-unnecessary-condition": "warn",
+
+    "import/no-extraneous-dependencies": "error",
+    "import/consistent-type-specifier-style": ["warn", "prefer-inline"],
   },
 };
