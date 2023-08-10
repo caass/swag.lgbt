1a3dae3b6ac4d2fee3a253ec6cafdfb818b8a6e7
Cass Fridkin <cass@swag.lgbt>
2023-08-09 23:27:29 -0400

    make it work with postcss and scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,38 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
+import { defineConfig, type ConfigEnv, type CSSOptions } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+
+import autoprefixer from "autoprefixer";
+import cssnanoPlugin from "cssnano";
+import postcssPrefixEnv from "postcss-preset-env";
+import scss from "postcss-scss";
+
+const defineCssConfig = ({ command }: ConfigEnv): CSSOptions => {
+  const postcssPlugins = [
+    autoprefixer(),
+    postcssPrefixEnv({ stage: 3, features: { "nesting-rules": true } }),
+  ];
+
+  // only minimize in prod
+  if (command === "build") {
+    postcssPlugins.push(cssnanoPlugin({ preset: "default" }));
+  }
+
   return {
+    postcss: {
+      parser: scss.parse,
+      plugins: postcssPlugins,
+    },
+  };
+};
+
+export default defineConfig((env) => {
+  const css = defineCssConfig(env);
+
+  return {
+    css,
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
