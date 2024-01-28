244857f3e79fc164d2466ab3ae3dcee3228a1629
Cass Fridkin <cass@swag.lgbt>
2024-01-27 21:04:32 -0500

    Add fallback font

--- src/root.tsx
+++ src/root.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head";
 
-import "@fontsource/vt323";
+import "@fontsource/vt323/latin-400.css";
 
 import "./global.scss";
 import { FlagProvider } from "./components/flag-provider";
